9af0c43462726e2d3abceb2d92b8aa41cc6a12b5
maxijaeger <maximilian-esternberg@gmx.at>
2019-11-21 16:45:26 +0100

    new cool changes, exact date difference from year to second

--- tageszaehler/timer/views.py
+++ tageszaehler/timer/views.py
@@ -1,5 +1,6 @@
-
+from datetime import datetime as dt
 from django.utils import timezone
+from django.utils.timesince import timesince
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login as lgin
 from django.shortcuts import render, redirect
@@ -8,19 +9,50 @@ from .models import DateTimeData
 
 from .forms import LoginForm
 
+
+def func_years(from_date, to_date):
+    if from_date is None:
+        from_date = datetime.now()
+    
+    from_date_year = from_date.year
+    to_date_year = to_date.year
+    diff = from_date_year - to_date_year
+    return diff
+    
+def func_months(from_date, to_date):
+    if from_date is None:
+        from_date = datetime.now()
+    
+    from_date_year = from_date.month
+    to_date_year = to_date.month
+    diff = from_date_year - to_date_year
+    return diff
+
 @login_required
 def home(request):
 
 	now = timezone.now()
 	start = DateTimeData.objects.get(id=1).datetime
-	print(start)
 	
-	print(now)
-	diff = now - start
+	
+	years = func_years(from_date=now, to_date=start)
+
+	months_fix = func_months(from_date=now, to_date=start) + years * 12
+
+	months_relative = func_months(from_date=now, to_date=start)
+
+
+	diff_fix = (now - start)
+
+	hours_fix = diff_fix.days * 24
+	min_fix = diff_fix.days * 24 * 60
+	sec_fix = diff_fix.days * 24 * 60 * 60
 
 
 
-	context = {'now': now, 'start': start, 'diff': diff}
+
+	context = {'now': now, 'start': start, 'diff': diff_fix, 'years': years, 'months_fix': months_fix, 'hours_fix': hours_fix,
+	'min_fix': min_fix, 'sec_fix': sec_fix}
 	return render(request, 'timer/home.html', context= context)
 
 
